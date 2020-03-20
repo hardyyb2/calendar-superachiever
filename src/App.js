f9bc7275c560f59fe5d027d694dfe2312f9c8036
hardyyb3 <heartz.sd3@gmail.com>
2020-03-20 09:39:47 +0530

    calendar done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+import Calendar from './Calendar/Calendar'
+import { Button, Grid } from '@material-ui/core'
+
+import useEventListener from '@use-it/event-listener'
+
+const SHOWCALENDAR_KEYS = ['65', 'A', 'a']
+
+const App = props => {
+  const [showCalendar, setShowCalendar] = useState(false)
+
+  function handler({ key }) {
+    if (SHOWCALENDAR_KEYS.includes(String(key))) {
+      setShowCalendar(true)
+    }
+    if (SHOWCALENDAR_KEYS.includes(String(key)) && showCalendar) {
+      setShowCalendar(false)
+    }
+  }
+
+  useEventListener('keydown', handler)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        showCalendar ?
+          <Calendar />
+          :
+          <Grid container
+            style={{
+              height: '100vh',
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+          >
+            <Button variant="contained" color="secondary">
+              Press A To Open Calendar
+          </Button>
+          </Grid>
+      }
     </div>
   );
 }
 
-export default App;
+export default App
